891af74a1c14f44f7745241b79e4e576c140747b
Destiny-wei <weihaoming0418@gmail.com>
2023-11-08 21:13:15 +0800

    project init

---
+++ tests/Counter.spec.ts
@@ -0,0 +1,38 @@
+import { Blockchain, SandboxContract } from '@ton-community/sandbox';
+import { Cell, toNano } from 'ton-core';
+import { Counter } from '../wrappers/Counter';
+import '@ton-community/test-utils';
+import { compile } from '@ton-community/blueprint';
+
+describe('Counter', () => {
+    let code: Cell;
+
+    beforeAll(async () => {
+        code = await compile('Counter');
+    });
+
+    let blockchain: Blockchain;
+    let counter: SandboxContract<Counter>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        counter = blockchain.openContract(Counter.createFromConfig({}, code));
+
+        const deployer = await blockchain.treasury('deployer');
+
+        const deployResult = await counter.sendDeploy(deployer.getSender(), toNano('0.05'));
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: counter.address,
+            deploy: true,
+            success: true,
+        });
+    });
+
+    it('should deploy', async () => {
+        // the check is done inside beforeEach
+        // blockchain and counter are ready to use
+    });
+});
